5f539bc3e860d2c0586f7f97325680575ad84d1a
mikebychkov <mikebychkov@github.com>
2021-01-15 15:20:04 +0500

    docker files update

--- licensing-service/src/main/docker/run.sh
+++ licensing-service/src/main/docker/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 echo "********************************************************"
-echo "Starting License Server"
+echo "Starting Licensing Service"
 echo "********************************************************"
-java -jar /usr/local/licensingservice/@project.build.finalName@.jar
+
+dockerize -wait http://configserver:8888 -wait http://database:5432
+
+java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
