812d997051f8578722300f18d7aec98689f72c1e
mikebychkov <mikebychkov@github.com>
2021-01-28 18:19:40 +0500

    docker properties

--- licensing-service/src/main/docker/run.sh
+++ licensing-service/src/main/docker/run.sh
@@ -3,9 +3,12 @@ echo "********************************************************"
 echo "Starting Licensing Service"
 echo "********************************************************"
 
-dockerize -wait http://eurekaserver:8761 -wait http://configserver:8888 -wait http://database:5432 -wait http://authenticationservice:8901
+dockerize -wait http://eurekaserver:8761 -wait http://configserver:8888 -wait http://database:5432
+dockerize -wait http://kafkaserver:2181 -wait http://redis:6379
 
 java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
+     -Dspring.cloud.stream.kafka.binder.zkNodes=$KAFKASERVER_URI          \
+     -Dspring.cloud.stream.kafka.binder.brokers=$ZKSERVER_URI             \
      -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
--- organization-service/src/main/docker/run.sh
+++ organization-service/src/main/docker/run.sh
@@ -3,10 +3,13 @@ echo "********************************************************"
 echo "Starting Organization Service  "
 echo "********************************************************"
 
-dockerize -wait http://eurekaserver:8761 -wait http://configserver:8888 -wait http://database:5432 -wait http://authenticationservice:8901
+dockerize -wait http://eurekaserver:8761 -wait http://configserver:8888 -wait http://database:5432
+dockerize -wait http://kafkaserver:2181
 
 java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
-     -Dspring.profiles.active=$PROFILE                                    \
+     -Dspring.profiles.active=$PROFILE                                   \
+     -Dspring.cloud.stream.kafka.binder.zkNodes=$KAFKASERVER_URI          \
+     -Dspring.cloud.stream.kafka.binder.brokers=$ZKSERVER_URI             \
      -jar /usr/local/organizationservice/@project.build.finalName@.jar
