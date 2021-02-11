acdca85587999ef51bdabdefb7e1b8a6112ca44e
mikebychkov <mikebychkov@github.com>
2021-02-12 00:35:03 +0500

    login to docker

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,5 +1,5 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
 
 ecs-cli configure profile --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
-ecs-cli configure --region eu-central-1 --cluster spmia-tmx-dev
+ecs-cli configure --region eu-central-1 --cluster spmia
 ecs-cli compose --file docker/common/docker-compose-aws.yml up
