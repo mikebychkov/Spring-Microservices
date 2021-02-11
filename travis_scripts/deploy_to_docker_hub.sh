7e308b2db9716e7db7a5ae6aa9822224bc2d5634
mikebychkov <mikebychkov@github.com>
2021-02-11 22:38:58 +0500

    configuring deployment to docker

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,10 +1,10 @@
 echo "Pushing service docker images to docker hub ...."
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
-docker push johncarnell/tmx-eurekasvr:$BUILD_NAME
-docker push johncarnell/tmx-confsvr:$BUILD_NAME
-docker push johncarnell/tmx-zuulsvr:$BUILD_NAME
-docker push johncarnell/tmx-authentication-service:$BUILD_NAME
-docker push johncarnell/tmx-organization-service:$BUILD_NAME
-docker push johncarnell/tmx-licensing-service:$BUILD_NAME
+docker push mikebychkov/tmx-eurekasvr:$BUILD_NAME
+docker push mikebychkov/tmx-confsvr:$BUILD_NAME
+docker push mikebychkov/tmx-zuulsvr:$BUILD_NAME
+docker push mikebychkov/tmx-authentication-service:$BUILD_NAME
+docker push mikebychkov/tmx-organization-service:$BUILD_NAME
+docker push mikebychkov/tmx-licensing-service:$BUILD_NAME
